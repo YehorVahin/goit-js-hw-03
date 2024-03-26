88e57e9f9b07efb4c6ecd865868643e46989cb72
YehorVahin <davyjohns322@gmail.com>
2024-03-26 14:53:20 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,9 @@
 'use strict';
 
-function makeTransaction(quantity, pricePerDroid, customerCredits){
-    const totalPrice = quantity * pricePerDroid;
-    if (totalPrice >= customerCredits) {
-        return `Insufficient funds!`;
-    }else {
-        return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-    }
+function slugify(title){
+    return title.toLowerCase().split(' ').join('-')
 }
+console.log(slugify("Arrays for begginers"));
+console.log(slugify("English for developer"));
+console.log(slugify("Ten secrets of JavaScript"));
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
