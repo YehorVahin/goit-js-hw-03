f6858888535d4e2b4f7c3bc25a7cad6f903411e6
YehorVahin <150614080+YehorVahin@users.noreply.github.com>
2024-03-26 14:43:44 +0200

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function formatMessage(message, maxLength) {
+   if (message.length <= maxLength) {
+    return message;
+   } else {
+    return message.slice(0, maxLength)+`...`;
+   }
+}
+console.log(formatMessage("Curabitur ligula sapien", 16));
---
+++ js/task-3.js
@@ -0,0 +1,6 @@
+'use strict';
+
+function checkForSpam(message){
+const lowerCaseMessage = message.toLowerCase();
+return lowerCaseMessage.includes(`spam`) || lowerCaseMessage.includes(`sale`);
+};
