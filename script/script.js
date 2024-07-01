15a1bf0a32367ca35262e4ceb70094ecf370ac18
YehorVahin <davyjohns322@gmail.com>
2024-07-01 21:05:14 +0300

    1

--- script/script.js
+++ script/script.js
@@ -9,5 +9,57 @@ const swiper = new Swiper('.swiper',{
 	spaceBetween: 100,
 	autoplay: {
 		delay: 5000,
+		observe: true,
+		observeParents: true,
+		observeChildren: true,
 	  },
 });
+const modalController = ({modal, btnOpen, btnClose}) => {	
+	const buttonElems = document.querySelectorAll(btnOpen);
+	const modalElems = document.querySelector(modal);
+
+	modalElems.style.cssText = `
+		display: flex;
+		visibility: hidden;
+		opasity: 0;
+		transition: opacity 300ms ease-in-out;
+		min-width: 0;
+	`;
+
+	const closeModal = event => {
+		const target = event.target;
+
+		if(target === modalElems || target.closest(btnClose)){
+			modalElems.style.opacity = 0;
+
+			setTimeout(() =>{
+				modalElems.style.visibility = 'hidden';
+			}, 300)
+		}
+	}
+
+	const openModal = () => {
+		modalElems.style.visibility = 'visible';
+		modalElems.style.opacity = 1;
+	};
+
+	buttonElems.forEach(btn => {
+		btn.addEventListener('click', openModal)
+	});
+
+	modalElems.addEventListener('click', closeModal)};
+	modalController({
+		modal: '.modal',
+		btnOpen: '.btn1',
+		btnClose: '.modal__close',
+	});
+	modalController({
+		modal: '.modal1',
+		btnOpen: '.btn2',
+		btnClose: '.modal__close',
+	});
+	modalController({
+		modal: '.modal2',
+		btnOpen: '.btn3',
+		btnClose: '.modal__close',
+	});
